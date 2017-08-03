4f027592726ca7abd19cbbc5536ff3faf91f551b
liuccbs <1085780173@qq.com>
2017-08-03 14:03:21 +0800

    冒泡排序

---
+++ src/main/java/com/testlog4j/www/test2.java
@@ -0,0 +1,35 @@
+package com.testlog4j.www;
+
+public class test2 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+	/*	int[] arr=new int [10];
+		arr[0]=2;
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}*/
+		
+		int[] arr =new int[]{1,2,3,42,7,3,56};
+		
+		for (int i = 0; i < arr.length-1; i++) {
+			
+			for (int j = 0; j < arr.length-1-i; j++) {
+				
+				if (arr[j]>arr[j+1]) {
+					int c=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=c;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
